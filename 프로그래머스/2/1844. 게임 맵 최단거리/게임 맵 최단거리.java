145f3648613c4fd28048035cbdfbdd2b05837208
Minji Kim <sml07113@gmail.com>
2025-11-20 18:47:17 +0900

    update: 행렬 기준으로 변경

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -4,24 +4,24 @@ class Solution {
     
     // 현재 위치를 객체로 정의
     private class Location {
-        final int x;
-        final int y;
+        final int r;    // 행 위치
+        final int c;    // 열 위치
         final int step; // 지금까지 움직인 칸 수
         
-        private Location(int x, int y, int step) {
-            this.x = x;
-            this.y = y;
+        private Location(int r, int c, int step) {
+            this.r = r;
+            this.c = c;
             this.step = step;
         }
     }
     
     // x, y좌표 움직임 정의 (상하좌우)
-    private static final int[] dx = {1, -1, 0, 0};
-    private static final int[] dy = {0, 0, -1, 1};
+    private static final int[] dr = {-1, 1, 0, 0};
+    private static final int[] dc = {0, 0, -1, 1};
     
     public int solution(int[][] maps) {        
-        int h = maps.length;    // 높이 (y축)
-        int w = maps[0].length; // 너비 (x축)
+        int h = maps.length;    // 높이 (행)
+        int w = maps[0].length; // 너비 (열)
         
         // 방문 여부
         boolean[][] isVisited = new boolean[h][w];
@@ -37,33 +37,32 @@ class Solution {
         while (!queue.isEmpty()) {
             // 큐에서 맨 앞(제일 먼저 넣은) 원소 추출
             Location nodeIndex = queue.poll();
-            // System.out.println(nodeIndex.x + " " + nodeIndex.y);
 
             // 현재 상태를 이용하는 로직 처리
-            if (nodeIndex.y == h - 1 && nodeIndex.x == w - 1) { // 목표 위치에 도달했을 때
+            if (nodeIndex.r == h - 1 && nodeIndex.c == w - 1) { // 목표 위치에 도달했을 때
                 // 지금까지 센 칸수 리턴
                 return nodeIndex.step;
             }
 
             // 다음 탐색 대상 찾기 => 동서남북 위치 순회
             for (int d = 0; d < 4; d++) {
-                int nx = nodeIndex.x + dx[d];
-                int ny = nodeIndex.y + dy[d];
+                int nr = nodeIndex.r + dr[d];
+                int nc = nodeIndex.c + dc[d];
                 
                 // 범위 검사 => 맵을 벗어나지 않는지
-                if (ny < 0 || ny >= h || nx < 0 || nx >= w) continue;
+                if (nr < 0 || nr >= h || nc < 0 || nc >= w) continue;
 
                 // 유효성 검사 => 맵의 좌표가 길(1)인지
-                if (maps[ny][nx] != 1) continue;
+                if (maps[nr][nc] != 1) continue;
 
                 // 중복 방문여부 검사
-                if (isVisited[ny][nx]) continue;
+                if (isVisited[nr][nc]) continue;
                 
                 // 방문 처리
-                isVisited[ny][nx] = true;
+                isVisited[nr][nc] = true;
 
                 // 인접 노드들을 탐색 공간(큐)에 추가
-                queue.add(new Location(nx, ny, nodeIndex.step + 1));
+                queue.add(new Location(nr, nc, nodeIndex.step + 1));
             }
         }
             
